423a2c5057e1543acea6f07eec129d80ef414bae
Alexis Bouhet <alexis@cher-ami.tv>
2018-11-23 17:29:06 +0100

    Patched doc for github.io

--- doczrc.js
+++ doczrc.js
@@ -1,12 +1,25 @@
-// doczrc.js
+
+// Getting base (repo name) from package.json
+const base = require(__dirname + '/tools/get-repo-base.js')('/');
+
+// With $ npm run doc:publish
+// Documentation will be deployed https://{organisation-name}.github.io/{repo-name}/
+
 export default
 {
-	src: './doc/',
+	// Configure title of doc here
 	title: '',
 	description: '',
+
+	// Configure theme here
+	// https://github.com/pedronauck/docz/tree/master/packages/docz-theme-default
 	themeConfig: {
 		colors: {
-			//primary: 'tomato',
 		}
-	}
+	},
+
+	// Config for github.io
+	base,
+	src: './doc/',
+  	hashRouter: true
 }
