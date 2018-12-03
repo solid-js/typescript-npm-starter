3d8b935776e37b80c4bd295b97a87957f01e5fac
Alexis Bouhet <alexis@cher-ami.tv>
2018-12-03 12:17:58 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-# TODO : Check .git exists
+# curl https://raw.githubusercontent.com/solid-js/typescript-npm-starter/master/tools/install.sh | sh
+
+inside_git_repo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
+if [ ! "$inside_git_repo" ]; then
+  echo "Please setup git repo in this folder first.";
+  exit 1;
+else
 
 echo "Downloading typescript-npm-starter archive..."
 curl -L -sS https://github.com/solid-js/typescript-npm-starter/archive/master.zip > archive.zip
@@ -11,9 +17,9 @@ echo "Installing archive..."
 unzip -q archive.zip
 rm archive.zip
 rm -rf typescript-npm-starter-master/README.md
-rm -rf typescript-npm-starter-master/LICENCE
+rm -rf typescript-npm-starter-master/LICENSE
+rm -rf typescript-npm-starter-master/install.sh
 mv typescript-npm-starter-master/* ./
-mv typescript-npm-starter-master/.docz ./
 cat typescript-npm-starter-master/.gitignore >> .gitignore
 rm -r typescript-npm-starter-master
 echo "Done !"
@@ -21,10 +27,5 @@ echo ""
 
 echo "Installing node dependencies..."
 npm install
-echo "Done !"
-echo ""
-
-echo "Removing install script..."
-rm -f tools/install.sh
 echo "Done !"
 echo ""
