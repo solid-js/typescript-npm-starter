200bc70d9402d0b38e6b2ce2267129b3db243833
Alexis Bouhet <alexis@cher-ami.tv>
2019-01-08 18:19:05 +0100

    Patched npmignore + removed dirty js files

--- src/lib/Other.js
+++
@@ -1,9 +0,0 @@
-var Other = /** @class */ (function () {
-    function Other() {
-    }
-    Other.prototype.test = function () {
-        return 12;
-    };
-    return Other;
-}());
-export { Other };
